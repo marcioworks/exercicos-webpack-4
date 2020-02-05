c46d134d06199ff03c71fb1c417ad849ff51d4f2
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-02-05 10:04:56 -0300

    extract css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
-const webpack = require('webpack')
+const Webpack = require('webpack')
+const MIniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode:'development',
-    entry:'./src/index.js',
-    output:{
-        filename:'principal.js',
-        path: __dirname +'/public'
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
     },
-    module:{
-        rules:[{
-            test:/\.css$/,
-            use:['style-loader',
-            'css-loader']
+    plugins: [
+        new MIniCssExtractPlugin({
+            filename: 'estilo.css'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MIniCssExtractPlugin.loader,
+                // 'style-loader',
+                'css-loader']
         }]
     }
 }
